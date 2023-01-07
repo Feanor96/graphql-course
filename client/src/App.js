464d2d0bee52c84cdcfe0322092941939ccfa6be
Salim <78372105+Feanor96@users.noreply.github.com>
2023-01-08 02:33:50 +0530

    added client

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import DisplayData from './DisplayData';
+
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
+
+const App = () => {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql'
+  });
+  return (
+    <ApolloProvider client={client}>
+      <div className="container">
+        <h2>List of users</h2>
+        <DisplayData />
+      </div>
+    </ApolloProvider>
+  )
+}
+
+export default App;
